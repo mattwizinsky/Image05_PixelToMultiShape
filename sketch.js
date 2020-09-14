5c35e0bf3d808df4ef28f0233a6e8b36f6b58e3c
Matt Wizinsky <mattwizinsky@gmail.com>
2020-09-14 13:01:46 -0400

    uploading p5 sketch files

---
+++ sketch.js
@@ -0,0 +1,50 @@
+// IMAGE PIXEL MULTISHAPE
+// Generative Design Systems
+// Matthew Wizinsky
+
+var img;
+
+function preload() {
+  img = loadImage('data/Chinatown-1974.jpg');
+}
+
+function setup() {
+  createCanvas(720, 480);
+  print(img.width + ' x ' + img.height);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(255);
+  img.loadPixels();
+  noStroke();
+  
+  // move mouse to change grid/pixel scale
+  var div = map(mouseX, 0, width, 10, 80);
+  
+  // nested loops to get/draw pixel locations
+  for (var gridX = 0; gridX < img.width / div; gridX++) {
+    for (var gridY = 0; gridY < img.height / div; gridY++) {
+  
+      // x & y position adjust by grid size
+      var x = div * gridX;
+      var y = div * gridY;
+
+      // GET current color
+      var c = img.get(x, y);
+      
+      // convert color to brightness (value)
+      var value = brightness(c);
+      fill(0);
+
+      // pick a shape-pixel based on value
+      if (value < 25) {
+        rect(x, y, div, div);
+      } else if ((value > 25) && (value < 50)) {
+        ellipse(x, y, div, div);
+      } else if ((value > 50) && (value < 75)) {
+        triangle(x - (div / 2), y + (div / 2), x + (div / 2), y + (div / 2), x, y - (div / 2));
+      }
+    }
+  }
+}
